1a4704f8bab7fc9b7b7ae07d80bbfd375022a833
Jino Antony <jinoantony99@gmail.com>
2020-04-25 18:24:16 +0530

    composer autoloading article, types of autoloading completed

--- src/templates/ArticleMeta.js
+++ src/templates/ArticleMeta.js
@@ -14,10 +14,23 @@ const ArticleMeta = props => (
 		</li>
 		<li className="readtime">
 			<Link className="time" to={props.link}>
-				{props.readTime}
+				{`${getFormattedDate(props.date)}・${props.readTime}`}
 			</Link>
 		</li>
 	</ul>
 )
+
+const getFormattedDate = date => {
+	let formatOption = {
+		day: "numeric",
+		month: "short",
+		year: "2-digit"
+	}
+
+	let formattedDate = new Date(date)
+		.toLocaleDateString("en-US", formatOption);
+
+	return formattedDate;
+}
 
 export default ArticleMeta
