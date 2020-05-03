2d1fea1dfd1b81a87eed2e741fc22c98fc94468a
Jino Antony <jinoantony99@gmail.com>
2020-05-03 23:33:55 +0530

    home page design changed

--- src/components/landing/Home/index.js
+++ src/components/landing/Home/index.js
@@ -2,22 +2,31 @@ import React from 'react'
 import me from '../../../../static/images/me.jpg'
 import SocialLinks from '../SocialLinks'
 import './Home.css'
+import RecentArticles from '../RecentArticles'
+import { Link } from 'gatsby'
 
 const Home = () => (
   <div className="box">
-    <img src={me} />
-    <h4>Hello, I am</h4>
-    <h1>Jino Antony</h1>
-    <p>
-      I'am a backend developer based in Kerala, India. I love all things related
-      to web development. I'm mostly experienced in PHP, Javascript and other
-      web related technologies.
-    </p>
-
-    <p>
-      You can find me on
-    </p>
-    <SocialLinks/>
+    <div className="my-info">
+      <div className="my-image">
+        <img src={me} />
+      </div>
+      <div className="intro">
+        <h4>Hi, I'm</h4>
+        <h1>Jino Antony</h1>
+      </div>
+    </div>
+    <div className="content">
+      <p className="about">
+        I am a backend developer based in Kerala, India. I love all things related
+        to web development. I'm mostly experienced in PHP, Javascript and other
+        web related technologies. I usually write articles related to web development.
+      </p>
+      <RecentArticles/>
+      <Link to="/blog">Read More</Link>
+    </div>
+    {/* <p>You can find me on</p>
+    <SocialLinks /> */}
   </div>
 )
 
---
+++ src/components/landing/RecentArticles/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { graphql, useStaticQuery, Link } from 'gatsby'
+
+const RecentArticles = () => {
+    const data = getData()
+
+    return (
+        <div className="latest-articles">
+            <h3>Latest Articles</h3>
+            <ul>
+                {data.allMarkdownRemark.edges.map(article => (
+                    <li>
+                        <Link to={article.node.frontmatter.slug}>
+                            { article.node.frontmatter.title }
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+const getData = () => useStaticQuery(graphql`
+    query RecentQuery {
+        allMarkdownRemark(
+            sort: { fields: [frontmatter___date], order: DESC }
+            limit: 5
+            filter: { frontmatter: { draft: {eq: false} }}
+        ) {
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        slug
+                        title
+                        date
+                    }
+                }
+            }
+        }
+    }
+`)
+
+export default RecentArticles
